6d46b54d3e15d57127e453da009ec77ddcf31e4c
poohlaha <>
2023-09-18 10:38:19 +0800

    add download process bar

--- packages/http/src/download.rs
+++ packages/http/src/download.rs
@@ -1,12 +1,18 @@
 //! 文件下载
 
+use std::cmp::min;
 use std::ffi::OsStr;
+use std::fmt::Write;
 use std::fs::{File};
 use std::io;
 use std::path::{Path, PathBuf};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use colored::*;
-use reqwest::{Client};
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
+use reqwest::{Client, Response};
+use reqwest::header::CONTENT_LENGTH;
+use tokio::fs::File as TokioFile;
+use tokio::io::AsyncWriteExt;
 use crate::LOGGER_PREFIX;
 
 pub struct Download;
@@ -21,31 +27,39 @@ pub struct DownloadOptions {
 const TIMEOUT: u64 = 30;
 impl Download {
 
-    /// 文件下载
-    pub async fn download(options: DownloadOptions) -> bool {
-        if options.url.is_empty() {
-            println!("{} download url is empty !", LOGGER_PREFIX.cyan().bold());
-            return false;
-        }
-
+    fn get_file_name(options: &DownloadOptions) -> String {
         let file_name = options.file_name.clone();
         let download_file_name;
         if file_name.is_none() {
-           let file_path = Path::new(&options.url);
+            let file_path = Path::new(&options.url);
             download_file_name = file_path.file_name().unwrap_or(OsStr::new("")).to_string_lossy().to_string();
         } else {
             download_file_name = file_name.unwrap();
         }
 
-        if download_file_name.is_empty() {
-            println!("{} download file name is empty, please check `url` or `file_name` !", LOGGER_PREFIX.cyan().bold());
-            return false;
-        }
+        return download_file_name;
+    }
 
+    fn get_timeout(options: &DownloadOptions) -> Duration {
         let timeout = options.timeout;
         let download_timeout = if timeout.is_none() { TIMEOUT } else {timeout.unwrap()};
+        return Duration::new(download_timeout, 0)
+    }
 
-        let client = match Client::builder().timeout(Duration::new(download_timeout, 0)).build() {
+    async fn get_response(options: &DownloadOptions) -> (Option<Response>, String) {
+        if options.url.is_empty() {
+            println!("{} download url is empty !", LOGGER_PREFIX.cyan().bold());
+            return (None, String::new());
+        }
+
+        let download_file_name= Download::get_file_name(&options);
+        if download_file_name.is_empty() {
+            println!("{} download file name is empty, please check `url` or `file_name` !", LOGGER_PREFIX.cyan().bold());
+            return (None, String::new());
+        }
+
+        let timeout = Download::get_timeout(&options);
+        let client = match Client::builder().timeout(timeout).build() {
             Ok(client) => Some(client),
             Err(err) => {
                 println!("{} create client error: {:#?}", LOGGER_PREFIX.cyan().bold(), err);
@@ -54,7 +68,7 @@ impl Download {
         };
 
         if client.is_none() {
-            return false;
+            return (None, String::new());
         }
 
         let client = client.unwrap();
@@ -66,7 +80,24 @@ impl Download {
             }
         };
 
-        if response.is_none() {
+        return (response, download_file_name);
+    }
+
+    fn get_output_file(options: &DownloadOptions, download_file_name: &str) -> PathBuf {
+        let mut output_file_path = PathBuf::new();
+        if options.output_dir.is_some() {
+            let output_dir = options.output_dir.clone().unwrap();
+            output_file_path.push(&output_dir);
+        }
+
+        output_file_path.push(&download_file_name);
+        return output_file_path;
+    }
+
+    /// 文件下载
+    pub async fn download(options: DownloadOptions) -> bool {
+        let (response, download_file_name) = Download::get_response(&options).await;
+        if response.is_none() || download_file_name.is_empty() {
             return false;
         }
 
@@ -75,7 +106,7 @@ impl Download {
             let content = match response.bytes().await {
                 Ok(content) => Some(content),
                 Err(err) => {
-                    println!("{} get response error: {:#?}", LOGGER_PREFIX.cyan().bold(), err);
+                    println!("{} download file {} error: {:#?}", LOGGER_PREFIX.cyan().bold(), &download_file_name, err);
                     None
                 }
             };
@@ -84,18 +115,12 @@ impl Download {
                 return false;
             }
 
-            let mut output_file_path = PathBuf::new();
-            if options.output_dir.is_some() {
-                let output_dir = options.output_dir.clone().unwrap();
-                output_file_path.push(&output_dir);
-            }
-
-            output_file_path.push(&download_file_name);
+            let output_file_path = Download::get_output_file(&options, &download_file_name);
             println!("{} download file path: {}", LOGGER_PREFIX.cyan().bold(), output_file_path.as_path().to_string_lossy().to_string());
             let dest_file = match File::create(output_file_path) {
                 Ok(file) => Some(file),
                 Err(err) => {
-                    println!("{} create file error: {:#?}", LOGGER_PREFIX.cyan().bold(), err);
+                    println!("{} download file {} error: {:#?}", LOGGER_PREFIX.cyan().bold(), &download_file_name, err);
                     None
                 }
             };
@@ -109,7 +134,7 @@ impl Download {
             let success = match io::copy(&mut content.as_ref(), &mut dest_file) {
                 Ok(_) => true,
                 Err(err) => {
-                    println!("{} download file error: {:#?}", LOGGER_PREFIX.cyan().bold(), err);
+                    println!("{} download file {} error: {:#?}", LOGGER_PREFIX.cyan().bold(), &download_file_name, err);
                     false
                 }
             };
@@ -118,12 +143,122 @@ impl Download {
                 return false;
             }
 
-            println!("{} download file successfully !", LOGGER_PREFIX.cyan().bold());
+            println!("{} download file {} successfully !", LOGGER_PREFIX.cyan().bold(), &download_file_name.cyan().bold());
             return true;
         } else {
-            println!("{} download file failed with status code: {}", LOGGER_PREFIX.cyan().bold(), response.status());
+            println!("{} download file {} failed with status code: {}", LOGGER_PREFIX.cyan().bold(), &download_file_name.cyan().bold(), response.status());
         }
 
         return false
+    }
+
+    fn format_size(size: u64) -> (f64, &'static str) {
+        if size < 1_000 {
+            (size as f64, "bytes")
+        } else if size < 1_000_000 {
+            (size as f64 / 1_000.0, "KB")
+        } else if size < 1_000_000_000 {
+            (size as f64 / 1_000_000.0, "MB")
+        } else {
+            (size as f64 / 1_000_000_000.0, "GB")
+        }
+    }
+
+    /// 文件下载, 包含进度条
+    pub async fn download_process(options: DownloadOptions) -> bool {
+        let (response, download_file_name) = Download::get_response(&options).await;
+        if response.is_none() {
+            return false;
+        }
+
+        let mut response = response.unwrap();
+        if !response.status().is_success() {
+            println!("{} download file {} failed with status code: {}", LOGGER_PREFIX.cyan().bold(), &download_file_name.cyan().bold(), response.status());
+            return false;
+        }
+
+        // 获取响应头中的文件大小
+        let content_length = response.headers().get(CONTENT_LENGTH).and_then(|value| value.to_str().ok())
+            .and_then(|value| value.parse::<u64>().ok())
+            .unwrap_or(0);
+
+        println!("{} file: {} content length: {}", LOGGER_PREFIX.cyan().bold(), &download_file_name, content_length);
+
+        if content_length == 0 {
+            println!("{} url: {} the file on the server is empty !", LOGGER_PREFIX.cyan().bold(), &options.url);
+            return false;
+        }
+
+        let output_file_path = Download::get_output_file(&options, &download_file_name);
+        println!("{} download file path: {}", LOGGER_PREFIX.cyan().bold(), output_file_path.as_path().to_string_lossy().to_string());
+
+        let file = match TokioFile::create(output_file_path).await {
+            Ok(file) => Some(file),
+            Err(err) => {
+                println!("{} create file error: {:#?}", LOGGER_PREFIX.cyan().bold(), err);
+                None
+            }
+        };
+
+        if file.is_none() {
+            return false;
+        }
+
+        let mut file = file.unwrap();
+        let mut downloaded_size = 0u64;
+        let mut time = Instant::now();
+        let mut download_speed = 0.0; // 下载速度
+
+        let pb = ProgressBar::new(content_length);
+        pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+            .unwrap()
+            .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
+            .progress_chars("#>-"));
+
+        // download
+        while let Some(chunk) = response.chunk().await.unwrap() {
+            let chunk_size = chunk.len() as u64;
+            downloaded_size += chunk_size;
+
+            let flag = match file.write_all(&chunk).await {
+                Ok(_) => true,
+                Err(err) => {
+                    println!("{} write to file {} error: {:#?}", LOGGER_PREFIX.cyan().bold(), &download_file_name.red().bold(), err);
+                    false
+                }
+            };
+
+            if !flag {
+                return false;
+            }
+
+            // 计算下载速度
+            let elapsed_time = time.elapsed().as_secs_f64();
+            if elapsed_time >= 1.0 {
+                download_speed = (downloaded_size as f64 / elapsed_time) / 1_000.0; // kbps
+                time = Instant::now();
+            }
+
+            // 更新进度条
+            pb.set_position(min(downloaded_size, content_length));
+        }
+
+        pb.finish_with_message("downloaded");
+
+        let success = match file.sync_all().await {
+            Ok(_) => true,
+            Err(err) => {
+                println!("{} download file {} error: {:#?}", LOGGER_PREFIX.cyan().bold(), &download_file_name, err);
+                false
+            }
+        };
+
+        if !success {
+            println!("{} download file {} failed !", LOGGER_PREFIX.cyan().bold(), &download_file_name.cyan().bold());
+            return false;
+        }
+
+        println!("{} download file {} successfully !", LOGGER_PREFIX.cyan().bold(), &download_file_name.cyan().bold());
+        return true;
     }
 }
