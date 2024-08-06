a6f14794c4678cce3756ab240171c4a6aec592ca
poohlaha <smile.love.tao@gmil.com>
2024-08-06 13:45:50 +0800

    update handlers, update zip package

--- packages/handlers/src/utils/mod.rs
+++ packages/handlers/src/utils/mod.rs
@@ -5,7 +5,7 @@ use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use log::info;
-use zip::write::FileOptions;
+use zip::write::{FileOptions, SimpleFileOptions};
 use zip::{CompressionMethod, ZipWriter};
 use crate::error::Error;
 use crate::file::FileHandler;
@@ -40,14 +40,14 @@ impl Utils {
         let relative_path = source_dir_path.strip_prefix(path.parent().unwrap()).unwrap_or(path).to_path_buf();
         let file = File::create(&path).map_err(|err| Error::Error(err.to_string()).to_string())?;
         let mut zip = ZipWriter::new(file);
-        let options = FileOptions::default().compression_method(CompressionMethod::Stored).unix_permissions(0o777);
+        let options = SimpleFileOptions::default().compression_method(CompressionMethod::Stored).unix_permissions(0o777);
         Self::add_directory_to_zip(&mut zip, &source_dir_path, &relative_path, &options)?;
         zip.finish().map_err(|err| Error::Error(err.to_string()).to_string())?;
         Ok(true)
     }
 
     /// 添加到 zip 包中
-    fn add_directory_to_zip(zip: &mut ZipWriter<File>, source_path: &Path, relative_path: &Path, options: &FileOptions) -> Result<(), String> {
+    fn add_directory_to_zip(zip: &mut ZipWriter<File>, source_path: &Path, relative_path: &Path, options: &FileOptions<()>) -> Result<(), String> {
         let entries = fs::read_dir(source_path).map_err(|err| Error::Error(err.to_string()).to_string())?;
         for entry in entries {
             let entry = entry.map_err(|err| Error::Error(err.to_string()).to_string())?;
@@ -55,7 +55,7 @@ impl Utils {
             let file_name = relative_path.join(entry.file_name());
 
             if path.is_file() {
-                // 添加文件到压缩包
+                // 添加文件到压缩包x
                 zip.start_file(file_name.to_str().unwrap(), *options).map_err(|err| Error::Error(err.to_string()).to_string())?;
                 let file_content = fs::read(path).map_err(|err| Error::Error(err.to_string()).to_string())?;
                 zip.write_all(&file_content).map_err(|err| Error::Error(err.to_string()).to_string())?;
