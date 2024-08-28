f722af2269810e91f3bc602e900ecf2554352f05
Kauren J <ztyjr88@163.com>
2024-08-28 16:35:36 +0800

    Add files via upload
    add docker

---
+++ packages/docker/src/docker.rs
@@ -0,0 +1,380 @@
+//! Docker, 可以使用第三方库 `bollard`
+
+use crate::error::Error;
+use handlers::command::func::CommandFuncHandler;
+use handlers::command::CommandHandler;
+use handlers::file::FileHandler;
+use handlers::utils::Utils;
+use log::{error, info};
+use sftp::sftp::SftpHandler;
+use ssh2::Session;
+use std::io::Read;
+use std::path::Path;
+use std::sync::{Arc, Mutex};
+
+pub struct DockerHandler;
+
+#[derive(Default, Debug, Clone)]
+pub struct DockerConfig {
+    dir: String,                  // 执行命令的目录
+    dockerfile: String,           // Dockerfile 文件
+    address: String,              // 远程仓库 address
+    image: String,                // docker image
+    version: String,              // docker image version
+    namespace: String,            // 远程仓库 namespace
+    user: String,                 // 远程仓库 用户名
+    password: String,             // 远程仓库 密码
+    need_push: String,            // 是否推送到远程仓库
+    nginx_path: String,           // docker 中 nginx 地址
+    nginx_content: String,        // docker 中 nginx 内容(如果需要拷贝 nginx)
+    platform: String,             // docker 打包平台
+    deploy_dir: String,           // 发布目录
+    kubernetes_namespace: String, // Kubernetes 命名空间名称
+}
+
+impl DockerConfig {
+    pub fn is_empty(config: &DockerConfig) -> bool {
+        if config.need_push == "Yes" {
+            return config.dir.is_empty()
+                || config.dockerfile.is_empty()
+                || config.image.is_empty()
+                || config.address.is_empty()
+                || config.namespace.is_empty()
+                || config.user.is_empty()
+                || config.password.is_empty()
+                || config.platform.is_empty()
+                || config.kubernetes_namespace.is_empty();
+        }
+
+        return config.dir.is_empty() || config.dockerfile.is_empty() || config.image.is_empty() || config.platform.is_empty();
+    }
+}
+
+impl DockerHandler {
+    pub(crate) async fn exec<F>(docker_config: &DockerConfig, func: F, server: &sftp::config::Server) -> Result<bool, String>
+    where
+        F: Fn(&str) + Send + Sync + Copy + 'static,
+    {
+        let msg = format!("docker config: {:#?}", docker_config);
+        func(&msg);
+
+        if DockerConfig::is_empty(&docker_config) {
+            return Err(Error::convert_string("run pipeline failed, `docker config some field` is empty!"));
+        }
+
+        // 判断本机有没有安装docker
+        let success = CommandHandler::check_installed_command("docker");
+        if !success {
+            return Err(Error::convert_string("no `docker` installed in os !"));
+        }
+
+        // 判断 docker 是否已启动
+        let str = CommandHandler::exec_command_result("docker info");
+        if str.is_empty() {
+            return Err(Error::convert_string("`docker` is not running !"));
+        }
+
+        // 获取 docker pull | docker push 命令
+        let mut commands: Vec<String> = Vec::new();
+        commands.push(format!("cd {}", docker_config.dir));
+
+        let time = Utils::get_date(Some("%Y%m%d%H%M%S".to_string()));
+
+        // 创建 nginx.conf 文件
+        let nginx_file_name = format!("nginx_{}.conf", time); // nginx 文件名
+        let nginx_file_path = Path::new(&docker_config.dir).join(&nginx_file_name);
+        let nginx_file_path_str = nginx_file_path.to_string_lossy().to_string();
+        FileHandler::write_to_file_when_clear(&nginx_file_path_str, &docker_config.nginx_content)?;
+
+        let mut dockerfile_content = docker_config.dockerfile.clone();
+
+        // 添加 nginx
+        if !docker_config.nginx_path.is_empty() && !docker_config.nginx_content.is_empty() {
+            let mut content: Vec<String> = dockerfile_content.lines().map(String::from).collect();
+            content.push(format!("ADD {} {}", nginx_file_name, docker_config.nginx_path));
+
+            if !docker_config.deploy_dir.is_empty() {
+                content.push(format!("COPY {} ./", docker_config.deploy_dir));
+            }
+
+            dockerfile_content = content.join("\n");
+        }
+
+        dockerfile_content.push_str("\n CMD [\"nginx\", \"-g\", \"daemon off;\"]");
+
+        // 创建 Dockerfile 文件
+        let dockerfile_file_name = format!("Dockerfile_{}", time); // dockerfile 文件名
+        let dockerfile_file_path = Path::new(&docker_config.dir).join(&dockerfile_file_name);
+        let dockerfile_file_path_str = dockerfile_file_path.to_string_lossy().to_string();
+        FileHandler::write_to_file_when_clear(&dockerfile_file_path_str, &dockerfile_content)?;
+        let msg = format!("Dockerfile content: \n{:#?}", dockerfile_content);
+        func(&msg);
+
+        let image = format!("{}/{}/{}:{}", docker_config.address, docker_config.namespace, docker_config.image, docker_config.version);
+        if docker_config.need_push == "Yes" {
+            let pull_nginx_command = Self::exec_docker_pull_nginx(&docker_config);
+            if pull_nginx_command.is_empty() {
+                FileHandler::delete_file(&dockerfile_file_path_str)?; // 删除 Dockerfile 文件
+                FileHandler::delete_file(&nginx_file_path_str)?; // 删除 nginx.conf 文件
+                return Err(Error::convert_string("can not get pull nginx command !"));
+            }
+
+            commands.push(format!("docker login {} --username {} --password {}", docker_config.address, docker_config.user, docker_config.password));
+            commands.push(pull_nginx_command);
+            commands.push(format!("docker buildx build -f ./{} -t {} --platform {} -o type=docker .", dockerfile_file_name, image, docker_config.platform));
+            commands.push(format!("docker push {}", image));
+        } else {
+            // 不需要推送，直接打本地包
+            commands.push(format!(
+                "docker buildx build -f ./{} -t {}:{} --platform {} -o type=docker .",
+                dockerfile_file_name, docker_config.image, docker_config.version, docker_config.platform
+            ));
+        }
+
+        let msg = format!("docker commands:\n{:#?}", commands);
+        func(&msg);
+
+        for command in commands.iter() {
+            let success = CommandFuncHandler::exec_command(&command, &docker_config.dir, move |msg| {
+                func(msg);
+            });
+
+            if !success {
+                FileHandler::delete_file(&dockerfile_file_path_str)?; // 删除 Dockerfile 文件
+                FileHandler::delete_file(&nginx_file_path_str)?; // 删除 nginx.conf 文件
+                return Err(Error::convert_string(&format!("run docker command failed: {}", command)));
+            }
+        }
+
+        FileHandler::delete_file(&dockerfile_file_path_str)?; // 删除 Dockerfile 文件
+        FileHandler::delete_file(&nginx_file_path_str)?; // 删除 nginx.conf 文件
+
+        info!("run docker commands success !");
+        if docker_config.need_push == "Yes" {
+            return Self::update_image(
+                &docker_config,
+                &image,
+                move |msg| {
+                    func(&msg);
+                },
+                server,
+            )
+            .await;
+        }
+
+        return Ok(true);
+    }
+
+    //  拉取 nginx 镜像 docker pull xxx
+    fn exec_docker_pull_nginx(docker_config: &DockerConfig) -> String {
+        let mut first_line = String::new();
+        let lines = docker_config.dockerfile.lines();
+        for line in lines.into_iter() {
+            if !first_line.is_empty() {
+                break;
+            }
+
+            first_line = line.to_string();
+        }
+
+        if first_line.is_empty() {
+            return String::new();
+        }
+
+        let first_line = first_line.trim();
+        if !first_line.starts_with("FROM ") {
+            error!("docker pull nginx error, not start with `FORM` ");
+            return String::new();
+        }
+
+        let command = first_line.replace("FROM", "docker pull");
+        info!("docker pull command: {}", command);
+        return command;
+    }
+
+    /// 连接服务器, 修改 image 地址
+    async fn update_image<F>(docker_config: &DockerConfig, image: &str, func: F, server: &sftp::config::Server) -> Result<bool, String>
+    where
+        F: Fn(&str) + Send + Sync + Copy + 'static,
+    {
+        let msg = "update `image` in `kubectl` ...";
+        func(&msg);
+
+        let func = |_: &str| {};
+        let log_func = Arc::new(Mutex::new(func));
+        let session = SftpHandler::connect(&server, log_func.clone())?;
+
+        // 登录到 root
+        let login_cmd = format!("echo {} | sudo -S -i", server.password);
+
+        // 获取当前 YAML 配置
+        let yaml_cmd = format!("kubectl get deploy {} -n {} -o yaml", docker_config.image, docker_config.kubernetes_namespace);
+        let cmd = format!("{} bash -c '{}'", login_cmd, yaml_cmd);
+        info!("get yaml config command: {}", cmd);
+
+        let yaml_content = Self::exec_remote_command(&session, &cmd, "get kubectl yaml config error", move |msg| {
+            func(&msg);
+        })?;
+
+        /*
+        let regex = Regex::new(r#"image:\s*([^\s]+)"#).unwrap();
+        let modified_yaml = regex.replace_all(&yaml_content, format!("image: {}", image).as_str());
+        */
+
+        let msg = format!("kubectl yaml content:\n{}", yaml_content);
+        func(&msg);
+
+        if yaml_content.is_empty() {
+            return Err(Error::convert_string("can not get kubectl yaml config !"));
+        }
+
+        let image_cmd = format!(
+            r#"{{"spec": {{
+                "template": {{
+                    "spec": {{
+                        "containers": [
+                            {{
+                                "name": "{}",
+                                "image": "{}"
+                                 "imagePullPolicy": "Always"
+                            }}
+                        ]
+                    }}
+                }}
+            }}
+        }}"#,
+            docker_config.image, image
+        )
+        .replace("\n", "") // 去除所有换行符
+        .replace("  ", ""); // 去除多余空格
+
+        let cmd = format!("{} kubectl patch deployment {} -n {} --type=merge --patch '{}'", login_cmd, docker_config.image, docker_config.kubernetes_namespace, image_cmd);
+
+        let msg = format!("kubectl update image command:\n{}", cmd);
+        func(&msg);
+
+        let output = Self::exec_remote_command(&session, &cmd, "exec command `kubectl patch` error", move |msg| {
+            func(&msg);
+        })?;
+
+        let msg = format!("kubectl patch output info: {}", output);
+        func(&msg);
+
+        let msg = "update `image` in `kubectl` success ...";
+        func(&msg);
+
+        // 如果没有 change, 需要删除原来的 pod
+        if output.contains("no change") {
+            let msg = "no change will delete pod ...";
+            func(&msg);
+
+            let success = Self::delete_pod_name(&session, docker_config, &login_cmd, move |msg| {
+                func(&msg);
+            })?;
+
+            if !success {
+                return Err(Error::convert_string("delete pod error!"));
+            }
+
+            let msg = "delete pod success ...";
+            func(&msg);
+        }
+
+        // 重启 pod: kubectl rollout restart deployment/xxx(image) -n xxx
+        let cmd = format!("{} kubectl rollout restart deployment/{} -n {}", login_cmd, docker_config.image, docker_config.kubernetes_namespace);
+        let msg = format!("pod restart command: {}", cmd);
+        func(&msg);
+
+        let output = Self::exec_remote_command(&session, &cmd, "exec command `kubectl patch` error", move |msg| {
+            func(&msg);
+        })?;
+
+        let msg = format!("pod restart command output info: {}", output);
+        func(&msg);
+
+        if output.is_empty() {
+            return Ok(false);
+        }
+
+        return Ok(true);
+    }
+
+    fn delete_pod_name<F>(session: &Session, docker_config: &DockerConfig, login_cmd: &str, func: F) -> Result<bool, String>
+    where
+        F: Fn(&str) + Send + Sync + Copy + 'static,
+    {
+        // 1. 查找 pod 名字 kubectl get pod -n xxx | grep xxx
+        let cmd = format!("{} kubectl get pod -n {} | grep {}", login_cmd, docker_config.kubernetes_namespace, docker_config.image);
+        let output = Self::exec_remote_command(session, &cmd, "kubectl get pod name error", move |msg| {
+            func(msg);
+        })?;
+
+        if output.is_empty() {
+            let msg = "no pod name output";
+            func(&msg);
+            return Ok(false);
+        }
+
+        let mut pod_name = String::new();
+        if let Some(line) = output.lines().find(|line| line.starts_with(&docker_config.image)) {
+            let name = line.split_whitespace().next().unwrap_or("");
+            let msg = format!("pod name: {}", name);
+            func(&msg);
+            pod_name = name.to_string()
+        } else {
+            let msg = "no pad name get !";
+            func(&msg);
+        }
+
+        if pod_name.is_empty() {
+            return Ok(false);
+        }
+
+        // 2. delete pod: kubectl delete pod -n xxx ${podname}
+        let cmd = format!("{} kubectl delete pod -n {} {}", login_cmd, docker_config.kubernetes_namespace, pod_name);
+        let mut output = Self::exec_remote_command(session, &cmd, "kubectl delete pod error", move |msg| {
+            func(msg);
+        })?;
+
+        let msg = format!("kubectl delete pod output: {}", output);
+        func(&msg);
+
+        output = output.trim().to_string();
+
+        if output.ends_with("deleted") {
+            return Ok(true);
+        }
+
+        return Ok(false);
+    }
+
+    /// 执行远程命令
+    fn exec_remote_command<F>(session: &Session, cmd: &str, error_msg: &str, func: F) -> Result<String, String>
+    where
+        F: Fn(&str) + Send + Sync + Copy + 'static,
+    {
+        let msg = format!("exec remote command: {}", cmd);
+        func(&msg);
+
+        let mut channel = SftpHandler::create_channel(&session)?;
+        channel.exec(&cmd).map_err(|err| {
+            let msg = format!("{}: {:#?}", error_msg, err);
+            error!("{}", &msg);
+            SftpHandler::close_channel_in_err(&mut channel);
+            Error::convert_string(&msg)
+        })?;
+
+        let mut output = String::new();
+        channel.read_to_string(&mut output).map_err(|err| {
+            let msg = format!("{}: {:#?}", error_msg, err);
+            error!("{}", &msg);
+            SftpHandler::close_channel_in_err(&mut channel);
+            Error::convert_string(&msg)
+        })?;
+
+        // PipelineRunnable::save_log(app, &format!("output info: {}", output), &pipeline.server_id, &pipeline.id, order);
+        SftpHandler::close_channel_in_err(&mut channel);
+
+        return Ok(output);
+    }
+}
