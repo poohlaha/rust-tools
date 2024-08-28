7602bde720943ef38da5f8afa8216044a35de821
poohlaha <smile.love.tao@gmil.com>
2024-08-28 17:04:51 +0800

    add docker package

--- packages/docker/src/docker.rs
+++ packages/docker/src/docker.rs
@@ -16,7 +16,7 @@ use std::sync::{Arc, Mutex};
 pub struct DockerHandler;
 
 impl DockerHandler {
-   pub async fn exec<F>(docker_config: &DockerConfig, server: &sftp::config::Server, func: F) -> Result<bool, String>
+    pub async fn exec<F>(docker_config: &DockerConfig, server: &sftp::config::Server, func: F) -> Result<bool, String>
     where
         F: Fn(&str) + Send + Sync + Copy + 'static,
     {
